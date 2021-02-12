631932d8e7af9c70772d7181653fc42e01f593ab
Rabin-Kalikote <rabinkalikote33@gmail.com>
2021-02-12 22:57:16 +0545

    Added game files.

---
+++ funtions.js
@@ -0,0 +1,17 @@
+
+const number = Math.floor((Math.random() * 20) + 1);
+
+document.getElementById("submit-guess").onclick = function() {
+  var guess = document.getElementById("guess").value;
+  var li = document.createElement('li');
+  if(guess > number) {
+    li.textContent = "You guess is too high."
+    document.getElementById("results").prepend(li);
+  } else if (guess < number) {
+    li.textContent = "You guess is too low."
+    document.getElementById("results").prepend(li);
+  } else{
+    li.textContent = "Congrats! Correct guess."
+    document.getElementById("results").prepend(li);
+  }
+};
